aa9227cd84885dbda48e6b82025335c45b4ee1d5
pasha <4pasha.com@gmail.com>
2018-08-30 12:44:59 +0300

    add client from regina

---
+++ src/main/java/ua/com/entity/Client.java
@@ -0,0 +1,34 @@
+package ua.com.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+//        (exclude = {"subCategory"})
+@EqualsAndHashCode
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idOfClient;
+    private String nameOfClient;
+    private String surNameOfClient;
+    private String emailOfClient;
+    private boolean isBuyer;
+    private boolean isSeller;
+    @OneToMany(
+            cascade = CascadeType.DETACH,
+            fetch = FetchType.EAGER,
+            mappedBy = "owner")
+    private List<Product> productListOfClient = new ArrayList<Product>();
+
+
+
+
+}
