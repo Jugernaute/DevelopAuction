6f6f711f4156f7dc0d61402bc6ed7b3a83d0241c
MykolaVanko <vankomykola@gmail.com>
2018-08-30 17:35:18 +0300

    make login and sing in

--- src/main/webapp/views/js/script.js
+++ src/main/webapp/views/js/script.js
@@ -1,20 +1,33 @@
 
 
-    var modal = $('.enter_form');
-    var btn = $('.enter');
-    var span = $('.close');
+    let loginForm = $('.enter_form');
+    let regForm = $('.registration_form');
+    let auction = $('.auction');
 
-    btn.on('click', function () {
-        modal.css('display', 'block')
+    $('.enter').on('click', function (e) {// animation for login form
+        loginForm.css('display', 'block');
+        regForm.css('display', 'none');
+        auction.addClass('blur');
     });
-    span.on('click',function () {
-        modal.css('display', 'none')
+
+    $('.registation').on('click', function () { //animation for registaration form
+        regForm.css('display', 'block');
+        loginForm.css('display', 'none');
+        $('.auction').addClass('blur');
+    });
+
+    $('.close').on('click',function () { // close login & registr form
+        loginForm.css('display', 'none');
+        regForm.css('display', 'none');
+        $('.auction').removeClass('blur');
+    });
+
+    $('.btn_alt_reg').on('click', function () { //open reg form from login form
+        loginForm.css('display', 'none');
+        regForm.css('display', 'block');
     })
-    $(window).on('click', function (event) {
-        console.log('sdfgse');
-        if (event.target == modal) {
-            modal.css('display', 'none')
-        }
-    })
+
+
+
 
 
