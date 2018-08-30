8a76c4a9e19f9ff04a13596a32b9083dfb43d92b
MykolaVanko <vankomykola@gmail.com>
2018-08-30 21:30:13 +0300

    add login page

--- src/main/webapp/views/js/script.js
+++ src/main/webapp/views/js/script.js
@@ -1,7 +1,38 @@
 
 
-    $('.registation').on('click',function () {
-        console.log('fgds');
-        $('.reg').toggleClass('dis_none');
-        $('.auction').toggleClass('blur');
+    let loginForm = $('.enter_form');
+    let regForm = $('.registration_form');
+    let auction = $('.auction');
+
+    $('.enter').on('click', function (e) {// animation for login form
+        loginForm.css('display', 'block');
+        regForm.css('display', 'none');
+        auction.addClass('blur');
     });
+
+    $('.registation').on('click', function () { //animation for registaration form
+        regForm.css('display', 'block');
+        loginForm.css('display', 'none');
+        $('.auction').addClass('blur');
+    });
+
+    $('.cancelbtn').on('click',function () {
+        regForm.css('display', 'none');
+        auction.removeClass('blur');
+    });
+
+    $('.close').on('click',function () { // close login & registr form
+        loginForm.css('display', 'none');
+        regForm.css('display', 'none');
+        auction.removeClass('blur');
+    });
+
+    $('.btn_alt_reg').on('click', function () { //open reg form from login form
+        loginForm.css('display', 'none');
+        regForm.css('display', 'block');
+    })
+
+
+
+
+
