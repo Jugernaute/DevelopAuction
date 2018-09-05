f975d90eda774415cea79138d5e97022df68dcae
MykolaVanko <vankomykola@gmail.com>
2018-09-05 14:06:10 +0300

    add password validation

--- src/main/webapp/views/js/script.js
+++ src/main/webapp/views/js/script.js
@@ -19,18 +19,38 @@
     $('.cancelbtn').on('click',function () {
         regForm.css('display', 'none');
         auction.removeClass('blur');
+        $(this).parent().parent().find('input').val('');
     });
 
     $('.close').on('click',function () { // close login & registr form
         loginForm.css('display', 'none');
         regForm.css('display', 'none');
         auction.removeClass('blur');
+        $(this).parent().parent().find('input').val('');
     });
 
     $('.btn_alt_reg').on('click', function () { //open reg form from login form
         loginForm.css('display', 'none');
         regForm.css('display', 'block');
-    })
+    });
+    
+    $('.signupbtn').on('click', function () {      //password validation
+        let psw = $(this).parent().parent().find('input[name="psw"]');
+        let pswRepeat = $(this).parent().parent().find('input[name="psw-repeat"]');
+        var passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{3,20}$/;
+        if (psw.val() != pswRepeat.val()){
+            pswRepeat.val('');
+            pswRepeat.prop('placeholder',"ПАРОЛІ НЕ ЗБІГАЮТЬСЯ!!!")
+
+        } else if (!psw.val().match(passw)){
+            psw.focus();
+            psw.val('');
+            $(this).parent().parent().find('#psw-must-have').css({
+                    'font-weight': 'bold',
+                    'color' : 'red'
+                })
+         }
+    });
 
 
 
