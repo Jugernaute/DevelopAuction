1a52ccdc032f023763d0e580adeb363e4036049a
pasha <4pasha.com@gmail.com>
2018-09-17 15:16:24 +0300

    merge from pasha

--- src/main/java/ua/com/dao/ClientDao.java
+++
@@ -1,8 +0,0 @@
-package ua.com.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ua.com.entity.Client;
-
-public interface ClientDao extends JpaRepository <Client, Integer> {
-
-}
