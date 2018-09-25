67d73c5e88f0fce1706f76675d07e97bc20a0b69
andriytrundey <t_andriy@ukr.net>
2018-09-25 15:45:59 +0300

    Test Beze

--- src/main/java/ua/com/method/Mail.java
+++ src/main/java/ua/com/method/Mail.java
@@ -1,39 +1,39 @@
-//package ua.com.method;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.mail.javamail.JavaMailSender;
-//import org.springframework.mail.javamail.MimeMessageHelper;
-//import org.springframework.stereotype.Component;
-//
-//import javax.mail.MessagingException;
-//import javax.mail.internet.MimeMessage;
-//
-//@Component
-//public class Mail {
-//
-//    @Autowired
-//    public JavaMailSender sender;
-//
-//    public void sendMail(String email, String subject, String text){
-//        MimeMessage mimeMessage = sender.createMimeMessage();
-//        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
-//        try {
-//            helper.setText(text, true);
-//        } catch (MessagingException e) {
-//            e.printStackTrace();
-//        }
-//        try {
-//            helper.setSubject(subject);
-//        } catch (MessagingException e) {
-//            e.printStackTrace();
-//        }
-//        try {
-//            helper.setTo(email);
-//        } catch (MessagingException e) {
-//            e.printStackTrace();
-//        }
-//        sender.send(mimeMessage);
-//    }
-//
-//
-//}
+package ua.com.method;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Component
+public class Mail {
+
+    @Autowired
+    public JavaMailSender sender;
+
+    public void sendMail(String email, String subject, String text){
+        MimeMessage mimeMessage = sender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
+        try {
+            helper.setText(text, true);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        try {
+            helper.setSubject(subject);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        try {
+            helper.setTo(email);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        sender.send(mimeMessage);
+    }
+
+
+}
